ab7a9d89e13d5cdd614cfe86016fa0aaa128f3fc
CsarrX <cesarpeam@gmail.com>
2024-10-12 23:50:58 -0600

    Correccion ejercicio 5

--- entrega2_modulo3/ejercicio5.c
+++ entrega2_modulo3/ejercicio5.c
@@ -2,42 +2,62 @@
 #include <math.h>
 
 int main() {
-    double monto_total_prestamo, tasa_interes_anual, num_years, pago_mensual;
-    double tasa_interes_mensual, num_total_pagos, balance_restante;
+  double montoPrestamo, tasaAnual, cuotaMensual, tasaMensual, balanceRestante;
+  int numAnios, numPagos, i, j;
 
-    printf("Introduce el monto total del prestamo: ");
-    scanf("%lf", &monto_total_prestamo);
-    if (monto_total_prestamo <= 0) {
-        printf("El monto del prestamo debe ser un valor positivo.\n");
-        return 1;
-    }
-
-    printf("Introduce la tasa de interes anual (en porcentaje): ");
-    scanf("%lf", &tasa_interes_anual);
-    if (tasa_interes_anual <= 0) {
-        printf("La tasa de interes anual debe ser mayor que cero.\n");
-        return 1;
-    }
-
-    printf("Introduce el numero de años para pagar el prestamo: ");
-    scanf("%lf", &num_years);
-
-    tasa_interes_mensual = (tasa_interes_anual / 100) / 12;
-    num_total_pagos = num_years * 12;
-
-    pago_mensual = (monto_total_prestamo * tasa_interes_mensual * pow(1 + tasa_interes_mensual, num_total_pagos)) / 
-                   (pow(1 + tasa_interes_mensual, num_total_pagos) - 1);
-    printf("Pago mensual: %.2lf\n", pago_mensual);
-
-    balance_restante = monto_total_prestamo;
-
-    for (int i = 0; i < num_years; i++) {
-        for (int j = 0; j < 12; j++) {
-            double interes_mensual = balance_restante * tasa_interes_mensual;
-            balance_restante -= (pago_mensual - interes_mensual);
-        }
-        printf("Año [%d], balance restante: %.2lf\n", i + 1, balance_restante);
-    }
-
+  // Solicitar datos al usuario
+  printf("Introduce el monto total del prestamo: ");
+  scanf("%lf", &montoPrestamo);
+  
+  // Validar que el monto del préstamo sea positivo
+  if (montoPrestamo <= 0) {
+    printf("El monto del prestamo debe ser positivo.\n");
     return 0;
+  }
+
+  printf("Introduce la tasa de interes anual (por ejemplo, 5 para 5%%): ");
+  scanf("%lf", &tasaAnual);
+  
+  // Validar que la tasa de interés sea mayor que cero
+  if (tasaAnual <= 0) {
+    printf("La tasa de interes debe ser mayor que cero.\n");
+    return 0;
+  }
+
+  printf("Introduce el numero de anos para pagar el prestamo: ");
+  scanf("%d", &numAnios);
+  
+  // Calcular tasa mensual y el número total de pagos
+  tasaMensual = (tasaAnual / 100) / 12;
+  numPagos = numAnios * 12;
+
+  // Calcular la cuota mensual
+  cuotaMensual = montoPrestamo * tasaMensual * pow(1 + tasaMensual, numPagos) / (pow(1 + tasaMensual, numPagos) - 1);
+  printf("La cuota mensual es: %.2lf\n", cuotaMensual);
+
+  // Inicializar el balance restante
+  balanceRestante = montoPrestamo;
+
+  // Ciclo externo para los años
+  for (i = 1; i <= numAnios; i++) {
+    // Ciclo interno para los meses dentro del año
+    for (j = 1; j <= 12; j++) {
+      // Calcular el interés mensual y lo que se paga al capital
+      double interesMes = balanceRestante * tasaMensual;
+      double pagoCapital = cuotaMensual - interesMes;
+      
+      // Actualizar el balance restante
+      balanceRestante -= pagoCapital;
+    }
+    // Mostrar el balance restante al final del año
+    printf("Balance restante al final del año %d: %.2lf\n", i, balanceRestante);
+    
+    // Si el balance es muy pequeño, detenerse
+    if (balanceRestante <= 0) {
+      balanceRestante = 0;
+      break;
+    }
+  }
+
+  return 0;
 }
